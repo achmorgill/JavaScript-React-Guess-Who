b6354cda81b1aee3c210f205bb741efda27af925
Gill Liddle <gillianmliddle49@gmail.com>
2017-07-14 16:41:32 +0100

    set up game structure

---
+++ client/src/containers/GameContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class GameContainer extends React.Component {
+  render() {
+    return(
+      <div>
+      hello
+      </div>
+      )
+  }
+  
+
+}
+export default GameContainer
